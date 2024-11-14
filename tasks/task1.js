1a4df3d80f094e9776451565c2438ce412b53457
timurpantus <timurmine2001@gmail.com>
2024-11-14 10:57:08 +0200

    Done

--- tasks/task1.js
+++ tasks/task1.js
@@ -4,10 +4,41 @@
 
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
-function fetchUsers() {
-  // Ваш код
-}
+const https = require('https');
 
+function fetchUsers() {
+    const options = {
+        hostname: 'jsonplaceholder.typicode.com',
+        path: '/users',
+        method: 'GET'
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const users = JSON.parse(data);
+                    const result = users.map(user => ({ id: user.id, name: user.name }));
+                    resolve(result);
+                } catch (error) {
+                    reject(error);  
+                }
+            });
+        });
+
+        req.on('error', (error) => {
+            reject(error);  
+        });
+
+        req.end();
+    });
+}
 console.log(fetchUsers())
 
 module.exports = fetchUsers;
