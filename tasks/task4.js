a388b13d02b32d9f06a5919f2815bc35990f7714
timurpantus <timurmine2001@gmail.com>
2024-11-14 10:58:34 +0200

    Done

--- tasks/task4.js
+++ tasks/task4.js
@@ -4,10 +4,30 @@
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
 
-function deleteUser(id) {
-  // Ваш код
+const https = require('https');
+
+function deleteUser(userId) {
+    const options = {
+        hostname: 'jsonplaceholder.typicode.com',
+        path: `/users/${userId}`,
+        method: 'DELETE'
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            resolve({ status: res.statusCode });
+        });
+        
+        req.on('error', (error) => {
+            reject(error);
+        });
+
+        req.end();
+    });
 }
 
-console.log(deleteUser(1));
+deleteUser(1)
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
 
 module.exports = deleteUser;
