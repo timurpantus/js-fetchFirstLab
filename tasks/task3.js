d77dca010d5b5c92211a000dae069982bc6b226f
timurpantus <timurmine2001@gmail.com>
2024-11-14 10:57:52 +0200

    Done

--- tasks/task3.js
+++ tasks/task3.js
@@ -5,8 +5,46 @@
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
 
+const https = require('https');
+
 function updateUser(id, updatedData) {
-  // Ваш код
+    const data = JSON.stringify(updatedData);
+
+    const options = {
+        hostname: 'jsonplaceholder.typicode.com',
+        path: `/users/${id}`,
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            let responseData = '';
+
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const response = JSON.parse(responseData);
+                    resolve(response);
+                } catch (error) {
+                    reject(error);  
+                }
+            });
+        });
+
+        req.on('error', (error) => {
+            reject(error);  
+        });
+
+        req.write(data);
+        req.end();
+    });
 }
 
 console.log(updateUser(1, { name: 'New Name' }));
